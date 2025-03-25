61985b413ada7a9f1239e41118489e9e538f72a1
nani-samireddy <nanisamireddy05@gmail.com>
2025-03-25 11:20:32 +0530

    initial commit

---
+++ Missing Number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+   long long n, size, sum=0, temp;
+   cin>>n;
+   size=n;
+   size--;
+   while(size--){
+    cin>>temp;
+    sum+=temp;
+   }
+   cout<< ((n * (n+1))/2 ) - sum;
+    return 0;
+}
---
+++ Weird Algorithm.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    long long n = 0;
+    cin >> n;
+
+    cout << n;
+
+    while (n!=1) {
+        if (n&1) {
+            n = (n*3) + 1;
+        } else {
+            n /=2;
+        }
+        cout << " " << n;
+    }
+
+    return 0;
+}
